7a2153ad59a8820c09f6ce610be0359c3e140453
ka1ex <kalexadamson@hotmail.com>
2021-06-10 12:20:28 -0500

    commit tree 7fc67ce540d305b920580c46a4eca2c7b5076d96

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+let EyesOnMe = () => {
+    
+    let good = () =>{
+        console.log('Good!')
+    }
+    let hey = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button 
+        onBlur= {() => console.log('Hey! Eyes on me!')} 
+        onFocus= {() => console.log('Good!')}>Eyes on me</button>
+   )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+
+let Keypad = () => {
+    return(
+        <input type='password'
+        onChange={() => console.log("Entering password...")}></input>
+    )
+}
+
+
+
+export default Keypad
+
